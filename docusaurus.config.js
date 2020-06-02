cb73c26f0b34cc28a0d79b73266d9a740049d282
Felipe Silva <felipe.dacs@gmail.com>
2020-06-02 02:15:01 -0300

    bump4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/docusaurus',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'facebook', // Usually your GitHub org/user name
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
